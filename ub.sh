632fd85600f2ee9ba5b269eaab30a3d302b0b9a7
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-09-26 14:42:48 +0530

    Add files via upload

---
+++ ub.sh
@@ -0,0 +1,13 @@
+apt update
+apt upgrade
+apt install wget proot python
+wget https://raw.githubusercontent.com/rooted-cyber/Anlinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh
+cat >> $PREFIX/bin/ub << EOF
+#!/data/data/com.termux/files/usr/bin/bash
+cd ~/Te*Ult*
+bash sta*ubu*
+EOF
+chmod 777 $PREFIX/bin/ub
+printf "\n Connect VPN\n"
+bash ubuntu.sh
+printf "\n \033[1;92m Now you can star ubuntu Type:\033[1;93m sub\n"
