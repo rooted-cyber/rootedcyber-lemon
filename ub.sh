6ae4a5c5232d5273002604d6821d1a3aa8b8560c
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-09-29 20:39:31 +0530

    Update ub.sh

--- ub.sh
+++ ub.sh
@@ -1,13 +1,41 @@
+pkc() {
+for a in wget proot python;do
+cd $PREFIX/bin
+if [ -e $a ];then
+printf "\n \033[3;93m already install\n"
+else
 apt update
 apt upgrade
-apt install wget proot python
-wget https://raw.githubusercontent.com/rooted-cyber/Anlinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh
-cat >> $PREFIX/bin/ub << EOF
+apt install $a || apt reinstall $a -y
+fi
+done
+}
+pkc
+chf() {
+cd ~
+if [ -e ubuntu.sh ];then
+printf "\n Connect VPN\n"
+bash ubuntu.sh
+rm $PREFIX/bin/sub > /dev/null 2>&1
+cat >> $PREFIX/bin/sub << EOF
 #!/data/data/com.termux/files/usr/bin/bash
 cd ~/Te*Ult*
 bash sta*ubu*
 EOF
-chmod 777 $PREFIX/bin/ub
+chmod 777 $PREFIX/bin/sub
+printf "\n \033[1;92m Now you can star ubuntu Type:\033[1;93m sub\n"
+else
+wget https://raw.githubusercontent.com/rooted-cyber/Anlinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh
 printf "\n Connect VPN\n"
 bash ubuntu.sh
+rm $PREFIX/bin/sub > /dev/null 2>&1
+cat >> $PREFIX/bin/sub << EOF
+#!/data/data/com.termux/files/usr/bin/bash
+cd ~/Te*Ult*
+bash sta*ubu*
+EOF
+chmod 777 $PREFIX/bin/sub
 printf "\n \033[1;92m Now you can star ubuntu Type:\033[1;93m sub\n"
+fi
+}
+chf
